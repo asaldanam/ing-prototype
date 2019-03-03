92ca12e6005de7601e13a47e7fcb2448cb237227
asaldana <asaldanam@gmail.com>
2019-03-04 00:33:31 +0100

    deploy

---
+++ docs/precache-manifest.9479dc1f6b52175dbef410962f3a1c21.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1694250f33800105fad12f794c092936",
+    "url": "/static/media/ic-24-white-back.1694250f.svg"
+  },
+  {
+    "revision": "4f8f88e77e3f26dd344e",
+    "url": "/static/css/main.61d69f02.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "985a3649d1e1a1e7a810edb77c5fce97",
+    "url": "/static/media/ic-16-white-arrowdown.985a3649.svg"
+  },
+  {
+    "revision": "c92224ee1e52b80b6961",
+    "url": "/static/js/2.c92224ee.chunk.js"
+  },
+  {
+    "revision": "4f8f88e77e3f26dd344e",
+    "url": "/static/js/main.4f8f88e7.chunk.js"
+  },
+  {
+    "revision": "f80bc17328c4a9cb0d64bbaacceefdd7",
+    "url": "/static/media/ic-24-sec-back.f80bc173.svg"
+  },
+  {
+    "revision": "9cd6d00a2732eab42f8cf363e74dd949",
+    "url": "/static/media/logo-white.9cd6d00a.svg"
+  },
+  {
+    "revision": "accbf63f5c72c23c3ec8043ed5a39f60",
+    "url": "/static/media/logo.accbf63f.svg"
+  },
+  {
+    "revision": "c92224ee1e52b80b6961",
+    "url": "/static/css/2.1387a8ab.chunk.css"
+  },
+  {
+    "revision": "bf79d6cf9aa6e1935a231dc9c5f6a976",
+    "url": "/index.html"
+  }
+];
